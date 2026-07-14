85c01605248f8861ee5f13cc137aee1bd6a7389a
LucasMendesBSantos <lucasmendes.brito@aluno.uece.br>
2026-07-14 01:16:54 -0300

    catch-all

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,1 +1,5 @@
-module.exports = require('../server')
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if (req.method === 'OPTIONS') return res.status(204).end()
+  res.json({ status: 'ok', message: "API Garrafão Rosa D'água online" })
+}
